a8f948a13c32338c6006ce37242742bd4d72ba25
Hanson227 <huangyunhan2003@gmail.com>
2022-02-14 00:12:23 +0800

    1

--- src/main.c
+++ src/main.c
@@ -7,19 +7,45 @@
 typedef unsigned char u8;
 typedef unsigned int u16;
 
-u8 Date;
+u8 KeyNum;
+u16 Num;
 
 int main()
 {
     LCD_Init();
-    LCD_ShowString(1,1,"Hello");
-    AT24C02_WriteByte(1,6);
-    Delay(5);
-    Date=AT24C02_ReadByte(1);
-    LCD_ShowNum(2,1,Date,3);
-    while (1)
-    {
-        
-    }
+	LCD_ShowNum(1,1,Num,5);
+	while(1)
+	{
+		KeyNum=Key();
+		if(KeyNum==1)	//K1按键，Num自增
+		{
+			Num++;
+			LCD_ShowNum(1,1,Num,5);
+		}
+		if(KeyNum==2)	//K2按键，Num自减
+		{
+			Num--;
+			LCD_ShowNum(1,1,Num,5);
+		}
+		if(KeyNum==3)	//K3按键，向AT24C02写入数据
+		{
+			AT24C02_WriteByte(0,Num%256);
+			Delay(5);
+			AT24C02_WriteByte(1,Num/256);
+			Delay(5);
+			LCD_ShowString(2,1,"Write OK");
+			Delay(1000);
+			LCD_ShowString(2,1,"        ");
+		}
+		if(KeyNum==4)	//K4按键，从AT24C02读取数据
+		{
+			Num=AT24C02_ReadByte(0);
+			Num|=AT24C02_ReadByte(1)<<8;
+			LCD_ShowNum(1,1,Num,5);
+			LCD_ShowString(2,1,"Read OK ");
+			Delay(1000);
+			LCD_ShowString(2,1,"        ");
+		}
+	}
     
 }
